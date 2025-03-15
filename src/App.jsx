89ba8b58ccfed9709a9bf4c74f3a53443433657f
Shashwat pratap singh <143164251+7SPS7@users.noreply.github.com>
2025-03-15 12:33:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,59 +2,63 @@ import './App.css'
 import Home from './Home';
 import Navbar from './Navbar';
 import Paste from './Paste';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 import View from './View';
 import Frontpage from './Frontpage';
 import Update from './Update';
 import Footer from './Footer';
 
-const router = createBrowserRouter(
-[
+const router = createBrowserRouter([
   {
     path: "/SyncNotes/",
-    element: 
-    <div>
-      <Navbar/>
-      <Frontpage/>
-      <Home/>
-      <Footer/>
-    </div>
+    element: (
+      <div>
+        <Navbar />
+        <Frontpage />
+        <Home />
+        <Footer />
+      </div>
+    )
   },
   {
-    path:"/SyncNotes/update/:id",
-    element: 
-    <div>
-      <Navbar/>
-      <Update/>
-      <Footer/>
-    </div>
+    path: "/SyncNotes/update/:id",
+    element: (
+      <div>
+        <Navbar />
+        <Update />
+        <Footer />
+      </div>
+    )
   },
   {
     path: "/SyncNotes/pastes",
-    element: 
-    <div>
-      <Navbar/>
-      <Paste/>
-      <Footer/>
-    </div>
+    element: (
+      <div>
+        <Navbar />
+        <Paste />
+        <Footer />
+      </div>
+    )
   },
   {
     path: "/SyncNotes/pastes/:id",
-    element: 
-    <div>
-      <Navbar/>
-      <View/>
-      <Footer/>
-    </div>
+    element: (
+      <div>
+        <Navbar />
+        <View />
+        <Footer />
+      </div>
+    )
+  },
+  // Redirect from "/" to "/SyncNotes/"
+  {
+    path: "/",
+    element: <Navigate to="/SyncNotes/" />
   }
-]
-);
+]);
 
 function App() {
-
-  return (
-    <RouterProvider router={router}/>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
